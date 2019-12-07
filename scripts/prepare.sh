792540604789093d8db733d5bc7eb68e7a84924d
Hung Tran <hungtran177@gmail.com>
2019-12-07 15:09:23 +0700

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,7 +8,6 @@ then
         exit 0
 fi
 ENV=""
-ECHO="false"
 
 while getopts ":he:p:b:" OPTION; do
         case $OPTION in
@@ -34,23 +33,30 @@ while getopts ":he:p:b:" OPTION; do
                         exit 0
                         ;;
 
+                *)
+                    echo "Option $1 is not a valid option."
+                    echo "Try './cmd.sh --help for more information."
+                    shift
+                    exit
+                    ;;
+
         esac
 done
 
-if [ -z $ENV  ] || [ -z $PROJECT_ID ]; then 
+if [ -z "$ENV"  ] || [ -z "$PROJECT_ID" ]; then 
     echo "Error: ENV or PROJECT_ID cannot be empty"
     exit 1
 fi
 
-if [ -z $BUCKET ];
+if [ -z "$BUCKET" ];
 then
     BUCKET=$PROJECT_ID-tfstate
     echo "No bucket defined. The bucket named $BUCKET will be selected as Terraform backend"
 fi
 
 #BUCKET=$PROJECT_ID-tfstate
-gcloud config set project $PROJECT_ID
-PROJECT_NUMBER="$(gcloud projects describe ${PROJECT_ID} --format='get(projectNumber)')"
+gcloud config set project "$PROJECT_ID"
+PROJECT_NUMBER="$(gcloud projects describe "${PROJECT_ID}" --format='get(projectNumber)')"
 TERRAFORM_SA=terraform
 TERRAFORM_SA_EMAIL=${TERRAFORM_SA}@${PROJECT_ID}.iam.gserviceaccount.com
 
@@ -72,7 +78,7 @@ do
     if [[ $(gcloud services list --enabled --format="value(NAME)" --filter="NAME:${api}" 2>&1) != "${api}"  ]]
     then   
         echo "Enabling ${api}"
-        gcloud services enable $api
+        gcloud services enable "$api"
     else 
         echo "$api is already enabled"
     fi
@@ -80,27 +86,37 @@ done
 
 
 # # Create Terraform service account
-saExisted=$(gcloud iam service-accounts list --format="value(email)" --filter="email:${TERRAFORM_SA_EMAIL}")
-if [ -z $saExisted ]
+if [[ $(gcloud iam service-accounts list --format="value(email)" --filter="email:${TERRAFORM_SA_EMAIL}" 2>&1) != "${TERRAFORM_SA_EMAIL}" ]]
 then 
+    echo "Create Terraform service account and grant the required permissions"
     gcloud iam service-accounts create terraform --description="Service Account for Terraform" --display-name="Terraform service account"
-    gcloud projects add-iam-policy-binding ${PROJECT_NUMBER} \
-        --member serviceAccount:${TERRAFORM_SA}@${PROJECT_ID}.iam.gserviceaccount.com \
+    gcloud projects add-iam-policy-binding "${PROJECT_NUMBER}" \
+        --member serviceAccount:"${TERRAFORM_SA_EMAIL}" \
         --role roles/owner > /dev/null
+    gcloud iam service-accounts keys create credentials.json --iam-account "${TERRAFORM_SA_EMAIL}"
 fi
 
 
 #Create GCS bucket as Terraform Backend
 echo "Create/configure a GCS Bucket as Terraform Backend"
-
-
-AVAIL=$(gsutil ls -p $PROJECT_ID | grep "${BUCKET}" | wc -l )
-if [ $AVAIL -eq 0 ]
+AVAIL=$(gsutil ls -p "$PROJECT_ID" | grep -c "${BUCKET}" )
+if [ "$AVAIL" -eq 0 ]
 then 
-    gsutil mb -p $PROJECT_ID gs://$BUCKET
-    gsutil versioning set on gs://$BUCKET
-    gsutil acl ch -u ${TERRAFORM_SA}@${PROJECT_ID}.iam.gserviceaccount.com:W gs://${BUCKET}
+    gsutil mb -p "$PROJECT_ID" gs://"$BUCKET"
+    gsutil versioning set on gs://"$BUCKET"
+    gsutil acl ch -u ${TERRAFORM_SA}@"${PROJECT_ID}".iam.gserviceaccount.com:W gs://"${BUCKET}"
 
 else
     echo "Bucket ${BUCKET} already existed"
 fi
+
+# Generate the predefined Terraform backend configuration
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$ROOT_DIR" || exit
+cat <<EOT > ../env/"${ENV}"/tf-backend.tf
+terraform {
+  backend "gcs" {
+    bucket = "${BUCKET}"
+  }  
+}
+EOT
